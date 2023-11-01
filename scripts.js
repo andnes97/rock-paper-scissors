3a21ff2ede30777a1adee5ed50b938f6db2b9ba9
Andreas Surface <107000401+andnes97@users.noreply.github.com>
2023-11-01 16:50:40 +1100

    Swap switch/case to if/else to make playRound function correctly

--- scripts.js
+++ scripts.js
@@ -1,39 +1,32 @@
 function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissor"];
     let computerSelection = options[Math.floor(Math.random() * options.length)];
-    console.log("computerSelection: " + computerSelection);
     
     return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
     let result = "No result";
-    switch (playerSelection, computerSelection) {
-        case "Rock", "Scissor":
-        case "Paper", "Rock":
-        case "Scissor", "Paper":
-            result = `You win! ${playerSelection} beats ${computerSelection}`;
-            break;
-        
-        case "Scissor", "Rock":
-        case "Rock", "Paper":
-        case "Paper", "Scissor":
-            result = `You lose! ${computerselection} beats ${playerSelection}`;
-            break;
-        
-        case "Rock", "Rock":
-        case "Paper", "Paper":
-        case "Scissor", "Scissor":
-            result = `It's a draw! You both selected ${playerSelection}`;
-            break;
-        
-        default:
-            result = "Something went wrong!";
-            break;
+    if ((playerSelection === "Rock" && computerSelection === "Scissor") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissor" && computerSelection === "Paper")) {
+        result = `You win! ${playerSelection} beats ${computerSelection}`;
+    
+    } else if ((playerSelection === "Scissor" && computerSelection === "Rock") ||
+        (playerSelection === "Rock" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Scissor")) {
+        result = `You lose! ${computerSelection} beats ${playerSelection}`;
+    
+    } else if (playerSelection === computerSelection) {
+        result = `It's a draw! You both selected ${playerSelection}`;
+    
+    } else {
+        result = "Something went wrong!";
     }
-    console.log(result);
+
     return result;
 }
-// return String announcing winner
 
-playRound();
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
