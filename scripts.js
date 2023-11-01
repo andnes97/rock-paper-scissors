bb79bd16669cb45e251b34e76b795f87126aa29e
Andreas Surface <107000401+andnes97@users.noreply.github.com>
2023-11-01 16:22:15 +1100

    Add playRound function to compare selections and return result string

--- scripts.js
+++ scripts.js
@@ -1,10 +1,39 @@
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissor"];
+    let options = ["Rock", "Paper", "Scissor"];
     let computerSelection = options[Math.floor(Math.random() * options.length)];
     console.log("computerSelection: " + computerSelection);
+    
     return computerSelection;
 }
 
-// playRound(playerSelection, computerSelection)
+function playRound(playerSelection, computerSelection) {
+    let result = "No result";
+    switch (playerSelection, computerSelection) {
+        case "Rock", "Scissor":
+        case "Paper", "Rock":
+        case "Scissor", "Paper":
+            result = `You win! ${playerSelection} beats ${computerSelection}`;
+            break;
+        
+        case "Scissor", "Rock":
+        case "Rock", "Paper":
+        case "Paper", "Scissor":
+            result = `You lose! ${computerselection} beats ${playerSelection}`;
+            break;
+        
+        case "Rock", "Rock":
+        case "Paper", "Paper":
+        case "Scissor", "Scissor":
+            result = `It's a draw! You both selected ${playerSelection}`;
+            break;
+        
+        default:
+            result = "Something went wrong!";
+            break;
+    }
+    console.log(result);
+    return result;
+}
 // return String announcing winner
 
+playRound();
